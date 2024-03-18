655347ffb3e2ae9ec889c610f9c472ffeab0db67
saisanthoshkonduri <konduri.santhosh96@gmail.com>
2024-03-18 14:23:29 -0500

    Added Qualifier annotation

--- src/main/java/com/demo/userblog/controller/HomeConroller.java
+++ src/main/java/com/demo/userblog/controller/HomeConroller.java
@@ -1,8 +1,9 @@
 package com.demo.userblog.controller;
 
 import com.demo.userblog.service.HomeService;
-import com.demo.userblog.service.ScreenServiceImpl;
+import com.demo.userblog.service.ServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,10 +13,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeConroller {
 
     @Autowired
+    @Qualifier("home")
     HomeService homeService;
     @Autowired
-    ScreenServiceImpl screenService;
-    @RequestMapping(value = "screen1", method = RequestMethod.GET)
+    @Qualifier("screen")
+    ServiceImpl screenService;
+    @RequestMapping(value = "screen", method = RequestMethod.GET)
     public String getHomeScreen(){
         homeService.print();
         screenService.setScreenName("Sai");
